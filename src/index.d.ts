215b9f132ad644d49b7b848b89746a3eea39f606
Begench <begenchgeldyev@gmail.com>
2024-04-07 12:58:05 +0700

    add: declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+declare module '*.png'
+declare module '*.sass' {
+    const content: Record<string, string>
+    export default content
+}
+
+declare module '*.svg' {
+    import React from 'react'
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+    export default SVG
+}
